6c3c877c0bf9fb26b801172fe247fb1356ff652e
kcsdesenvolvedor <kcs.desenvolvedor@gmail.com>
2021-12-23 14:59:03 -0300

    Commit final

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -23,7 +23,6 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
-import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
@@ -46,7 +45,6 @@ registerLocaleData(localePt);
     ForDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component,
     ProductUpdateComponent,
     ProductDeleteComponent
   ],
